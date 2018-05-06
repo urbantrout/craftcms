48414b48ce89a9119fe6ee566344071ade38e90d
Lukas Holzer <office@lukasholzer.com>
2018-05-06 12:40:02 +0200

    add possibility to add local composer plugins or from a git repo

--- run.sh
+++ run.sh
@@ -34,11 +34,40 @@ update_dependencies() {
 		composer remove ${removable[*]}
 	fi
 
-	if [ ${#require[*]} -gt 0 ]; then
-		h2 "Install packages"
-		echo ${require[*]}
-		composer require ${require[*]}
-	fi
+ 	# Regex pattern to match VCS dependencies (Git repos)
+  # external dependencies have to look like this pattern 
+  #   - [vendor/package-name:branch-name]https://url-to-the-git-repo.git
+  #   - [vendor/package-name:version]/path/to/volume
+  # If dependency is not registerd by composer
+  regexHttp="(\[(.*?):(.*?)\])([http].*)" 
+  regexLocal="(\[(.*?):(.*?)\])([^http].*)"
+
+  h2 'Start adding all dependencies'
+
+  for package in ${require[@]}; do
+
+      if [[ $package =~ $regexHttp ]]; then
+        name="${BASH_REMATCH[2]}"
+        version="${BASH_REMATCH[3]}"
+        url="${BASH_REMATCH[4]}"
+        printf "Found \e[0;32m vcs-composer package:\e[0m for: \e[0;36m ${url}\e[0m \n"
+        composer config repositories.${name} '{"type": "vcs", "url": "'${url}'", "no-api": true }'
+        composer require ${name}:${version}
+      elif [[ $package =~ $regexLocal ]]; then
+        name="${BASH_REMATCH[2]}"
+        version="${BASH_REMATCH[3]}"
+        url="${BASH_REMATCH[4]}"
+        printf "Found \e[0;32m local composer package:\e[0m for: \e[0;36m ${url}\e[0m \n"
+        composer config repositories.${name} '{"type": "path", "url": "'${url}'", "options": {"symlink": true}}'
+        composer require ${name}:${version}
+      else
+        printf "Require \e[0;32m composer package:\e[0m for: \e[0;36m${package}\e[0m\n"
+				composer require ${package}
+      fi
+
+  done
+
+  printf '✅ 📌 all dependencies where required '
 }
 
 import_database() {
