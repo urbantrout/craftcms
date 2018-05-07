af65f5c59edb4ed43ebd224b736c59ee487606b5
Harald Atteneder <harald@urbantrout.io>
2018-05-07 17:32:02 +0200

    Add finishing message

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,5 +8,7 @@ source /scripts/dependencies.sh && update_dependencies &
 
 source /scripts/database.sh && import_database &
 
+h2 "✅ All set. Visit http://localhost or http://<docker-machine-ip> to start using Craft CMS."
+
 # Start php-fpm
 exec "$@"
